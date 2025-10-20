1145b8e73a28172fb034fa9f22a4ab6b7f2a24dd
Samuel <samudofia91@gmail.com>
2025-10-20 21:50:29 +0100

    initial Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+  const timeElement = document.getElementById('user-time');
+
+    function updateTime() {
+      const timeInMs = Date.now();
+      timeElement.textContent = timeInMs;
+    }
+
+    // Update immediately and every second
+    updateTime();
+    setInterval(updateTime, 1000);
